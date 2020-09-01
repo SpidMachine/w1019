ca937a724b353a19a66d1ef3d72328b1da7e7d11
Nikita <56842259+SpidMachine@users.noreply.github.com>
2020-09-01 17:56:01 +0300

    newVersionDB

--- install/_DATABASE__.sql
+++ install/_DATABASE__.sql
@@ -2,10 +2,10 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Aug 18, 2020 at 07:07 PM
--- Server version: 10.3.22-MariaDB
--- PHP Version: 7.4.5
+-- Хост: 127.0.0.1:3306
+-- Время создания: Сен 01 2020 г., 17:53
+-- Версия сервера: 10.3.22-MariaDB
+-- Версия PHP: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `car_market`
+-- База данных: `car_market`
 --
 CREATE DATABASE IF NOT EXISTS `car_market` DEFAULT CHARACTER SET utf16 COLLATE utf16_bin;
 USE `car_market`;
@@ -26,35 +26,39 @@ USE `car_market`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `car`
+-- Структура таблицы `car`
 --
 
 CREATE TABLE `car` (
   `id` int(11) NOT NULL COMMENT 'Номер',
-  `year_of_issue` int(11) NOT NULL COMMENT 'Дата выпуска',
+  `name_of_car` varchar(200) NOT NULL COMMENT 'Название',
+  `year_of_issue` varchar(100) NOT NULL COMMENT 'Дата выпуска',
   `condition_car` varchar(100) NOT NULL COMMENT 'Состояние',
-  `mileage` int(11) NOT NULL COMMENT '\r\nПробег',
+  `mileage` varchar(100) NOT NULL COMMENT 'Пробег',
   `fuel_type` varchar(100) NOT NULL COMMENT 'Тип топлива',
-  `volume` int(11) NOT NULL COMMENT 'Объем л.',
+  `volume` varchar(11) NOT NULL COMMENT 'Объем л.',
   `color` varchar(100) NOT NULL COMMENT 'Цвет',
   `body_type` varchar(100) NOT NULL COMMENT 'Тип кузова',
   `transmission` varchar(100) NOT NULL COMMENT 'Тип коробки передач',
   `drive_unit` varchar(100) NOT NULL COMMENT 'Тип привода',
-  `price` int(11) NOT NULL COMMENT 'Цена',
-  `users_id` int(11) NOT NULL COMMENT 'Номер пользователя'
+  `price` varchar(100) NOT NULL COMMENT 'Цена',
+  `users_id` int(11) NOT NULL COMMENT 'Номер пользователя',
+  `image` varchar(100) NOT NULL COMMENT 'Изображение'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `car`
+-- Дамп данных таблицы `car`
 --
 
-INSERT INTO `car` (`id`, `year_of_issue`, `condition_car`, `mileage`, `fuel_type`, `volume`, `color`, `body_type`, `transmission`, `drive_unit`, `price`, `users_id`) VALUES
-(3, 12, '123', 123, '123', 123, '123', '123', '123', '123', 123, 3);
+INSERT INTO `car` (`id`, `name_of_car`, `year_of_issue`, `condition_car`, `mileage`, `fuel_type`, `volume`, `color`, `body_type`, `transmission`, `drive_unit`, `price`, `users_id`, `image`) VALUES
+(15, 'Дедовская', '1945', 'С пробегом', '1003400', 'дизель', '100', 'радужный', 'Купешечка', 'Механика ', 'Задний', '3400', 3, 'public.jpeg'),
+(18, 'Джип', '2007', 'с пробегом', '235000', 'бензин', '3700', 'серебристый', 'внедорожник', 'Автомат', 'Постоянный полный', '22644', 5, 'public_16716276_b_dc6f400.jpeg'),
+(19, 'BMW 3 серия F30, F31 BMW 3 серия F30 328 xdrive, 2015', '2015', 'с пробегом', '64000 км', 'бензин', '2000 см3', 'черный', 'седан', 'Автомат', 'Постоянный полный', '51 796 р', 4, 'public_18718341_b_a1a845d.jpeg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `group`
+-- Структура таблицы `group`
 --
 
 CREATE TABLE `group` (
@@ -64,7 +68,7 @@ CREATE TABLE `group` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `group`
+-- Дамп данных таблицы `group`
 --
 
 INSERT INTO `group` (`id`, `name`, `cod`) VALUES
@@ -74,28 +78,27 @@ INSERT INTO `group` (`id`, `name`, `cod`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `guestbook`
+-- Структура таблицы `guestbook`
 --
 
 CREATE TABLE `guestbook` (
   `id` int(11) NOT NULL COMMENT '№',
   `text` text COLLATE utf16_bin NOT NULL COMMENT 'Текст',
-  `phonenumber` varchar(50) COLLATE utf16_bin NOT NULL COMMENT 'Телефон',
-  `email` varchar(50) COLLATE utf16_bin NOT NULL COMMENT 'Почта',
   `name` varchar(50) COLLATE utf16_bin NOT NULL COMMENT 'Имя'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
 
 --
--- Dumping data for table `guestbook`
+-- Дамп данных таблицы `guestbook`
 --
 
-INSERT INTO `guestbook` (`id`, `text`, `phonenumber`, `email`, `name`) VALUES
-(1, 'aaa', 'dd', 'ff@mail.ru', 'dd');
+INSERT INTO `guestbook` (`id`, `text`, `name`) VALUES
+(1, 'aaa', 'dd'),
+(2, '', '2222');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Структура таблицы `users`
 --
 
 CREATE TABLE `users` (
@@ -109,7 +112,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `users`
+-- Дамп данных таблицы `users`
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `group_id`, `FIO`, `city`, `phone`) VALUES
@@ -118,14 +121,17 @@ INSERT INTO `users` (`id`, `login`, `password`, `group_id`, `FIO`, `city`, `phon
 (4, 'Ivan', '432f45b44c432414d2f97df0e5743818', 1, 'Иванов Иван Иванович', '', ''),
 (5, 'Peter', '2b7c0e435a43e9c7453c8063b1ac2358', 1, 'Петров Петр Петрович', '', ''),
 (6, 'Sidorov', '2836a7191f6a9e2455b763469ca4ec6d', 1, 'Сидоров', '', ''),
-(30, 'Nikita', '155cc4b3ca33ea30480903c2f411a5a2c4a05363', 1, 'Ковалев Н.С.', 'Витебск', '123123');
+(33, 'asdasd', '5fc8cf618051513019be6bf621986d1aede1b729', 1, 'Валерка Борисов', 'Винница', '+04322225464'),
+(34, 'Nikita', '4d0f4a64104d422ee024a786d52136170e53f60c', 1, 'Ковалев Н.С.', 'Витебск', '+375290000000'),
+(35, 'Arkash321', '4d0f4a64104d422ee024a786d52136170e53f60c', 1, 'Аркадий', '', ''),
+(36, 'Gosha123', 'cf3cf62065e9acb6208a23c8a643ebc6f9b8f8ae', 2, 'Гоша', '', '');
 
 --
--- Indexes for dumped tables
+-- Индексы сохранённых таблиц
 --
 
 --
--- Indexes for table `car`
+-- Индексы таблицы `car`
 --
 ALTER TABLE `car`
   ADD PRIMARY KEY (`id`),
@@ -133,20 +139,20 @@ ALTER TABLE `car`
   ADD KEY `user_id` (`users_id`);
 
 --
--- Indexes for table `group`
+-- Индексы таблицы `group`
 --
 ALTER TABLE `group`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id` (`id`);
 
 --
--- Indexes for table `guestbook`
+-- Индексы таблицы `guestbook`
 --
 ALTER TABLE `guestbook`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Индексы таблицы `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
@@ -154,45 +160,45 @@ ALTER TABLE `users`
   ADD KEY `group_id` (`group_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
--- AUTO_INCREMENT for table `car`
+-- AUTO_INCREMENT для таблицы `car`
 --
 ALTER TABLE `car`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Номер', AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Номер', AUTO_INCREMENT=20;
 
 --
--- AUTO_INCREMENT for table `group`
+-- AUTO_INCREMENT для таблицы `group`
 --
 ALTER TABLE `group`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '№', AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `guestbook`
+-- AUTO_INCREMENT для таблицы `guestbook`
 --
 ALTER TABLE `guestbook`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '№', AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '№', AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '№', AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '№', AUTO_INCREMENT=38;
 
 --
--- Constraints for dumped tables
+-- Ограничения внешнего ключа сохраненных таблиц
 --
 
 --
--- Constraints for table `car`
+-- Ограничения внешнего ключа таблицы `car`
 --
 ALTER TABLE `car`
   ADD CONSTRAINT `car_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `users`
+-- Ограничения внешнего ключа таблицы `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `group` (`id`);
